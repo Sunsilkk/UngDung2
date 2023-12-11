5546695534c5379ba143b476a19ce875114fdb73
KhimCho <95128995+KhimCho@users.noreply.github.com>
2023-12-11 15:41:47 +0700

    Update Dashboard.java

--- Wheather/app/src/main/java/com/example/logintest/Dashboard.java
+++ Wheather/app/src/main/java/com/example/logintest/Dashboard.java
@@ -28,6 +28,7 @@ public class Dashboard extends AppCompatActivity {
         setContentView(binding.getRoot());
        
 
+
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
